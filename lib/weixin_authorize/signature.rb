6fc77035ce6e3241fa1677e250d98c63e80c0c50
youjin <youjin.guo@letote.cn>
2022-04-21 16:42:45 +0800

    sign

---
+++ lib/weixin_authorize/signature.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+module WeixinAuthorize
+  class Sign
+    def self.hexdigest(token, timestamp, nonce, msg_encrypt)
+      array = [token, timestamp, nonce]
+      array << msg_encrypt unless msg_encrypt.nil?
+      dev_msg_signature = array.compact.collect(&:to_s).sort.join
+      Digest::SHA1.hexdigest(dev_msg_signature)
+    end
+
+    def self.verify?(signature, token, timestamp, nonce, msg_encrypt)
+      return true unless signature
+      signature == WeixinAuthorize::Sign.hexdigest(token, timestamp, nonce, msg_encrypt)
+    end
+  end
+end
